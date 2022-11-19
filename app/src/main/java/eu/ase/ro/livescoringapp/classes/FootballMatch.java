9e7b49a47cfe36a73248a1a072c52372d659d206
GeorgeOrzanescu <george.asro@gmail.com>
2022-11-19 16:15:35 +0200

    Added Football match class

---
+++ app/src/main/java/eu/ase/ro/livescoringapp/classes/FootballMatch.java
@@ -0,0 +1,43 @@
+package eu.ase.ro.livescoringapp.classes;
+
+public class FootballMatch {
+    private Integer id;
+    private String homeTeam;
+    private String awayTeam;
+
+
+    public FootballMatch(Integer id, String homeTeam, String awayTeam) {
+        this.id = id;
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getHomeTeam() {
+        return homeTeam;
+    }
+
+    public void setHomeTeam(String homeTeam) {
+        this.homeTeam = homeTeam;
+    }
+
+    public String getAwayTeam() {
+        return awayTeam;
+    }
+
+    public void setAwayTeam(String awayTeam) {
+        this.awayTeam = awayTeam;
+    }
+
+    @Override
+    public String toString() {
+        return "Match: " + id + " " + getHomeTeam() + " : " + getAwayTeam();
+    }
+}
